2f2d3c5efedde87483ce7d4f1771e7c1b274cd41
Daniel Macura <daniel.macura@gmail.com>
2023-10-15 03:35:29 +0200

    ci: :sparkles: Test now procude JUnit XML testresults.xml

--- tests/test.py
+++ tests/test.py
@@ -1,45 +1,40 @@
-#write a program that iterates over all of the sub folders in the current directory and for each, runs the file in provided in the arguments for each input file in the folder
-#usage: python3 test.py <program to run>
 import os
-import sys
 import subprocess
+import sys
+import xml.etree.ElementTree as ET
 
-def main():
-    #number of tests
-    num_tests = 0
-    #number of tests passed
-    num_passed = 0
-    if len(sys.argv) != 2:
-        print("usage: python3 test.py <program to run>")
-        exit(1)
-    program = sys.argv[1]
-    #iterate over all subfolders and print them
+def run_tests(program):
+    testsuite = ET.Element("testsuite", name="Test Suite", tests="0", failures="0", errors="0", skipped="0")
     for path, subdirs, files in os.walk("."):
         for name in files:
-            #for each file ending in .in, run the program with the file as input and chcek if the output equals the .out file
             if name.endswith(".in"):
-                #increment the number of tests
-                num_tests += 1
-                #if the files are the same, increment the number of tests passed
-                expected_rc = int(open(os.path.join(path, name[:-3] + ".rc"), "r").read())
-                actual_rc = subprocess.call([program, "-t"], stdin=open(os.path.join(path, name)), stdout=open(os.path.join(path, name[:-3] + ".temp"), "w"))
+                test_name = name[:-3]
+                classname = path.replace('/', '.')[2:]
+                testcase = ET.Element("testcase", name=test_name, classname=classname, time="0")
+                expected_rc = int(open(os.path.join(path, test_name + ".rc"), "r").read())
+                actual_rc = subprocess.call([program, "-t"], stdin=open(os.path.join(path, name)), stdout=open(os.path.join(path, test_name + ".temp"), "w"))
                 if actual_rc != expected_rc:
-                    print("Test " + name[:-3] + " failed with return code " + str(actual_rc) + ". Expected return code " + str(expected_rc))
-                    continue
-            
-                if expected_rc == 0:
-                    if subprocess.call(["diff", os.path.join(path, name[:-3] + ".temp"), os.path.join(path, name[:-3] + ".out")]) != 0:
-                        print("Test " + name[:-3] + " failed")
-                        continue
-                        
-                num_passed += 1
+                    failure = ET.Element("failure", message="Return code does not match expected value", type="AssertionError", text=f"Expected return code {expected_rc}, but got {actual_rc}")
+                    testcase.append(failure)
+                    testsuite.set("failures", str(int(testsuite.get("failures")) + 1))
+                elif expected_rc == 0:
+                    diff_rc = subprocess.call(["diff", os.path.join(path, test_name + ".temp"), os.path.join(path, test_name + ".out")])
+                    if diff_rc != 0:
+                        failure = ET.Element("failure", message="Output does not match expected value", type="AssertionError", text="Output does not match expected value")
+                        testcase.append(failure)
+                        testsuite.set("failures", str(int(testsuite.get("failures")) + 1))
+                if len(testcase.findall("failure")) == 0:
+                    testcase.set("time", "0.001")
+                testsuite.set("tests", str(int(testsuite.get("tests")) + 1))
+                testsuite.append(testcase)
+                os.remove(os.path.join(path, test_name + ".temp"))
+    with open("testresults.xml", "w") as f:
+        f.write(ET.tostring(testsuite, encoding="unicode", method="xml"))
+    print(f"Tests passed: {int(testsuite.get('tests')) - int(testsuite.get('failures'))}/{testsuite.get('tests')}")
 
-                #remove the temp file
-                os.system("rm " + os.path.join(path, name[:-3] + ".temp"))
-                #print a newline
-    #print the number of tests passed and the number of tests
-    print("Tests passed: " + str(num_passed) + "/" + str(num_tests))
-
-
-
-main()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: python3 test_runner.py <program to run>")
+        exit(1)
+    program = sys.argv[1]
+    run_tests(program)
