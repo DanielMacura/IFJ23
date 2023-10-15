4482b0e31379f2a20da6b77396b4d25255edd10f
Daniel Macura <daniel.macura@gmail.com>
2023-10-15 19:39:48 +0200

    ci: :hammer: add timeout for tests

--- tests/test.py
+++ tests/test.py
@@ -12,17 +12,23 @@ def run_tests(program):
                 classname = path.replace('/', '.')[2:]
                 testcase = ET.Element("testcase", name=test_name, classname=classname, time="0")
                 expected_rc = int(open(os.path.join(path, test_name + ".rc"), "r").read())
-                actual_rc = subprocess.call([program, "-t"], stdin=open(os.path.join(path, name)), stdout=open(os.path.join(path, test_name + ".temp"), "w"))
-                if actual_rc != expected_rc:
-                    failure = ET.Element("failure", message="Return code does not match expected value", type="AssertionError", text=f"Expected return code {expected_rc}, but got {actual_rc}")
-                    testcase.append(failure)
-                    testsuite.set("failures", str(int(testsuite.get("failures")) + 1))
-                elif expected_rc == 0:
-                    diff_rc = subprocess.call(["diff", os.path.join(path, test_name + ".temp"), os.path.join(path, test_name + ".out")])
-                    if diff_rc != 0:
-                        failure = ET.Element("failure", message="Output does not match expected value", type="AssertionError", text="Output does not match expected value")
+                try:
+                    actual_rc = subprocess.call([program, "-t"], stdin=open(os.path.join(path, name)), stdout=open(os.path.join(path, test_name + ".temp"), "w"), timeout=1)
+
+                    if actual_rc != expected_rc:
+                        failure = ET.Element("failure", message="Return code does not match expected value", type="AssertionError", text=f"Expected return code {expected_rc}, but got {actual_rc}")
                         testcase.append(failure)
                         testsuite.set("failures", str(int(testsuite.get("failures")) + 1))
+                    elif expected_rc == 0:
+                        diff_rc = subprocess.call(["diff", os.path.join(path, test_name + ".temp"), os.path.join(path, test_name + ".out")])
+                        if diff_rc != 0:
+                            failure = ET.Element("failure", message="Output does not match expected value", type="AssertionError", text="Output does not match expected value")
+                            testcase.append(failure)
+                            testsuite.set("failures", str(int(testsuite.get("failures")) + 1))
+                except subprocess.TimeoutExpired:
+                    failure = ET.Element("failure", message="Test timed out", type="TimeoutError", text="Test timed out")
+                    testcase.append(failure)
+                    testsuite.set("failures", str(int(testsuite.get("failures")) + 1))
                 if len(testcase.findall("failure")) == 0:
                     testcase.set("time", "0.001")
                 testsuite.set("tests", str(int(testsuite.get("tests")) + 1))
