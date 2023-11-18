9897d04914afb74148d4dc40ddf3d4b07aa911cf
Daniel Macura <daniel.macura@gmail.com>
2023-11-18 13:54:31 +0100

    feat: :sparkles: Add doubly linked list for storing tokens.

---
+++ src/DLL.c
@@ -0,0 +1,76 @@
+/**
+ * Project: IFJ23 Compiler
+ * 
+ * @file DLL.c
+ * @brief Implementation of doubly linked list
+ * 
+ * @authors xmacur09
+*/
+
+#include "DLL.h"
+#include <stdio.h>
+
+
+DLL *createDLL() {
+    DLL *newDLL = malloc(sizeof(DLL));
+    newDLL->firstElement = NULL;
+    newDLL->activeElement = NULL;
+    newDLL->lastElement = NULL;
+    return newDLL;
+}
+
+
+void DLL_push(DLL *dll, token *t) {
+    DLLElementPtr new_element = malloc(sizeof(*new_element));
+    new_element->data = *t;
+    new_element->previousElement = dll->lastElement;
+    new_element->nextElement = NULL;
+    if (dll->lastElement != NULL) {
+        dll->lastElement->nextElement = new_element;
+    }
+    else {
+        dll->firstElement = new_element;
+    }
+    dll->lastElement = new_element;
+    dll->activeElement = dll->lastElement;
+}
+
+
+token *DLL_get_active(DLL *dll) {
+    if (dll->activeElement != NULL) {
+        return &dll->activeElement->data;
+    }
+    return NULL;
+}
+
+
+void DLL_move_active_left(DLL *dll) {
+    if (dll->activeElement == NULL) {
+        return;
+    }
+    dll->activeElement = dll->activeElement->previousElement;
+}
+
+
+void DLL_move_active_right(DLL *dll) {
+    if (dll->activeElement == NULL) {
+        dll->activeElement = dll->firstElement;
+        return;
+    }
+    dll->activeElement = dll->activeElement->nextElement;
+}
+
+
+void DLL_free(DLL *dll) {
+    DLLElementPtr current = dll->firstElement;
+    DLLElementPtr next;
+    while (current != NULL) {
+        next = current->nextElement;
+        if (current->data.VAL.string!= NULL /*&& current->data.ID !=TOKEN_ID_KEYWORD*/) { //keywords are not allocated      TODO: fix me probably
+            free(current->data.VAL.string);
+        }
+        free(current);
+        current = next;
+    }
+    free(dll);
+}
---
+++ src/DLL.h
@@ -0,0 +1,74 @@
+/**
+ * Project: IFJ23 Compiler
+ * 
+ * @file DLL.h
+ * @brief Interface for doubly linked list
+ * 
+ * @authors xmacur09
+*/
+
+#ifndef DLL_H
+#define DLL_H
+
+#include "token.h"
+#include <stdlib.h>
+
+typedef struct DLLElement {
+    token data;
+    struct DLLElement *previousElement;
+    struct DLLElement *nextElement;
+} * DLLElementPtr;
+
+typedef struct {
+    DLLElementPtr firstElement;
+    DLLElementPtr activeElement;
+    DLLElementPtr lastElement;
+} DLL;
+
+/**
+ * @brief Creates new doubly linked list
+ * 
+ * @return DLL* 
+ */
+DLL *createDLL();
+
+/**
+ * @brief Pushes new token to the end of the list
+ * 
+ * @param dll 
+ * @param t 
+ */
+void DLL_push(DLL *dll, token *t);
+
+token *DLL_pop(DLL *dll);
+
+/**
+ * @brief Returns token from the end of the list
+ * 
+ * @param dll 
+ * @return token* 
+ */
+token *DLL_get_active(DLL *dll);
+
+/**
+ * @brief Moves active element to the left
+ * 
+ * @param dll 
+ */
+void DLL_move_active_left(DLL *dll);
+
+/**
+ * @brief Moves active element to the right
+ * 
+ * @param dll 
+ */
+void DLL_move_active_right(DLL *dll);
+
+/**
+ * @brief Frees the list
+ * 
+ * @param dll 
+ */
+void DLL_free(DLL *dll);
+
+#endif
