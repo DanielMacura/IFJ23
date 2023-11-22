ab25f580522f81707e22bc25bf46cd23843b61cc
Daniel Macura <daniel.macura@gmail.com>
2023-11-22 12:43:16 +0100

    fix: Extract data types to avoid cyclic redundancy.

---
+++ src/types.h
@@ -0,0 +1,43 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef enum {
+    TERM,
+    NONTERM,
+    DOLLAR
+} expr_item_type;
+
+typedef enum {
+    UNDEFINED,
+    NULL_TYPE,
+    INT,
+    INT_NULL,
+    FLOAT,
+    FLOAT_NULL,
+    STRING,
+    STRING_NULL,
+    BOOL_TYPE,
+    TERM_TYPE,       //INT, FLOAT or STRING
+    
+} data_type;
+
+typedef enum rules{
+    PAR_E_PAR,      // E -> (E)
+    E_PLUS_E,       // E -> E + E
+    E_MINUS_E,      // E -> E - E
+    E_CONCAT_E,     // E -> E + E
+    E_MULTIPLE_E,   // E -> E * E
+    E_DIVIDE_E,     // E -> E / E
+    E_EQ_E,         // E -> E == E
+    E_NEQ_E,        // E -> E != E
+    E_LT_E,         // E -> E < E
+    E_GT_E,         // E -> E > E
+    E_LEQ_E,        // E -> E <= E
+    E_GEQ_E,        // E -> E >= E
+    E_COALESCE_E,   // E -> E ?? E
+    E_EXCLAMATION,  // E -> E!
+    ID,             // E -> id
+    NONE            // No rule
+} rules;
+
+#endif
