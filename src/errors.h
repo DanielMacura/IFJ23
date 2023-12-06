c1a21c360536ed72266510072cb45933b69b052c
Daniel Macura <daniel.macura@gmail.com>
2023-12-06 12:56:53 +0100

    docs: :bulb: Update commennts documenting errors.

--- src/errors.h
+++ src/errors.h
@@ -15,7 +15,7 @@ typedef enum error_code {
     SUCCESS,                // 0
     LEXICAL_ERR,            // 1 - chyba programu v ramci lexikalni analyzy (chybna struktura aktualniho lexemu)
     SYNTAX_ERR,             // 2 - chyba v proramu v ramci syntakticke analyzy (chybna syntaxe programu)
-    UNDEFINED_FUNCTION_ERR, // 3 - semanticka chyba v programu - nedefinovana funkce
+    UNDEFINED_FUNCTION_ERR, // 3 - semanticka chyba v programu - nedefinovana funkce, redefinicia premennej
     PARAMETERS_ERR,         // 4 - spatny pocet/typ parametru u volani funkce ci typ navratove hodnoty z funkce
     UNDEFINED_VAR_ERR,      // 5 - nedefinovana promenna
     RETURN_EXPR_ERR,        // 6 - chybejici/prebyvajici vyraz v prikazu navratu z funkce
